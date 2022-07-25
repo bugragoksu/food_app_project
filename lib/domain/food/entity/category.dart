0e5d376381eaa8bd747c60c3a699dbb43f916895
Buğra Göksu <bugra.goksu@parasut.com>
2022-07-26 00:43:46 +0300

    add category entity

---
+++ lib/domain/food/entity/category.dart
@@ -0,0 +1,11 @@
+class Category {
+  Category({
+    required this.id,
+    required this.imageUrl,
+    required this.name,
+  });
+
+  final int id;
+  final String imageUrl;
+  final String name;
+}
